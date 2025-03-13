876f67eeb6ee07800ecc1548b787b6b2f65b43bd
heenathakur8 <heena01thakur@gmail.com>
2025-03-13 14:23:03 +0530

    login backend

---
+++ backend/Routes/AuthRouter.js
@@ -0,0 +1,13 @@
+const {signupValidation}=require('../Middlewares/AuthValidation');
+const {loginValidation}=require('../Middlewares/AuthValidation');
+const {signup}=require('../Controllers/AuthController');
+const {login}=require('../Controllers/AuthController');
+const router =require('express').Router();
+
+router.post('/login',(req,res)=>{
+    res.send('login success');
+
+});
+router.post('/login',loginValidation,login);
+router.post('/signup',signupValidation,signup);
+module.exports=router;
