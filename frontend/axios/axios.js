ab1e55453d04798938108ce2c5202738c2b3e2aa
Ritesh Thakur <134767629+riteshh-thakur@users.noreply.github.com>
2025-03-20 17:30:36 +0530

    Update axios.js

--- frontend/axios/axios.js
+++ frontend/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with custom configuration
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8080', // Replace with your API base URL
+    baseURL: 'https://hackathontesting-56gs.onrender.com', // Replace with your API base URL
     timeout: 10000,
 });
 
